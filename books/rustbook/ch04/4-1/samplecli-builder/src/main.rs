6b24bc11ddc457d70cc841a5f6e23a8f6e7fbc6a
gawao <gawao57@gmail.com>
2023-02-10 23:56:31 +0900

    P.148-149 builder pattern

--- books/rustbook/ch04/4-1/samplecli-builder/src/main.rs
+++ books/rustbook/ch04/4-1/samplecli-builder/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("My RPN program")
+        .version("1.0.0")
+        .author("Your name")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            arg!(<FILE> "Formulas written in RPN")
+                .index(1)
+                .required(false),
+        )
+        .arg(arg!(-v --verbose "Sets the level of verbosity").required(false))
+        .get_matches();
+
+    match matches.get_one::<String>("FILE") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = matches.get_flag("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
