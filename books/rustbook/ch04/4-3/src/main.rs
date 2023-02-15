f4cddbad38089cdcb13d6dd39e1772b9eb1d31a5
gawao <gawao57@gmail.com>
2023-02-16 00:14:23 +0900

    P.167 逆ポーランド記法計算機テスト

--- books/rustbook/ch04/4-3/src/main.rs
+++ books/rustbook/ch04/4-3/src/main.rs
@@ -100,7 +100,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_ok() {
+    fn test_ok_sample() {
         assert_eq!(2 * 2, 4);
+    }
+
+    #[test]
+    fn test_ok() {
+        let calc = RpnCalculator::new(false);
+        assert_eq!(calc.eval("5"), 5);
+        assert_eq!(calc.eval("50"), 50);
+        assert_eq!(calc.eval("-50"), -50);
+
+        assert_eq!(calc.eval("2 3 +"), 5);
+        assert_eq!(calc.eval("2 3 *"), 6);
+        assert_eq!(calc.eval("2 3 -"), -1);
+        assert_eq!(calc.eval("2 3 /"), 0);
+        assert_eq!(calc.eval("2 3 %"), 2);
     }
 }
