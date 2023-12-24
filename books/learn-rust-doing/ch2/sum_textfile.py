818a1728d04032f5f8b705d88dd80e6c79166110
gawao <gawao57@gmail.com>
2023-12-24 23:40:48 +0900

    P.171 Pythonでテキストファイル内の数値を合計するツール

---
+++ books/learn-rust-doing/ch2/sum_textfile.py
@@ -0,0 +1,18 @@
+import sys
+total = 0
+# コマンドラインに指定したファイルを全て処理
+for i, v in enumerate(sys.argv):
+    if i == 0:
+        continue
+    # テキストファイルを読む
+    with open(v, 'r') as fp:
+        text = fp.read()
+        # 改行を区切って読む
+        for line in text.split('\n'):
+            try:
+                total += float(line)
+            except ValueError:
+                pass;
+# 結果を表示
+print(total)
+
