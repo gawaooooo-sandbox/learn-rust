21c3dbe28733e8fe02fd9741d7abaa4133831f0f
gawao <gawao57@gmail.com>
2024-01-14 16:22:51 +0900

    P.312 ジェネリクスの復習

---
+++ books/learn-rust-doing/ch4/vec_i32_char.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // i32型のVecを使う
+    let mut v1: Vec<i32> = Vec::<i32>::new();
+    v1.push(10);
+    v1.push(20);
+    v1.push(30);
+    v1.pop(); // 末尾の要素を取り出す
+
+    // 要素を繰り返す
+    for i in v1.iter() {
+        println!("{}", i);
+    }
+
+    // char型のVecを使う
+    let mut v2: Vec<char> = Vec::<char>::new();
+    v2.push('a');
+    v2.push('b');
+    v2.push('c');
+    v2.pop(); // 末尾の要素を取り出す
+
+    // 要素を繰り返す
+    for i in v2.iter() {
+        println!("{}", i);
+    }
+}
