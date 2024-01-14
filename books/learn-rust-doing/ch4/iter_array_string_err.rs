7b60e2585e0f1b9b60b6308c153a5f18bc2a5766
gawao <gawao57@gmail.com>
2024-01-15 07:28:54 +0900

    P.323 所有権の問題でfor文がうまくいかない例

---
+++ books/learn-rust-doing/ch4/iter_array_string_err.rs
@@ -0,0 +1,15 @@
+// 問題があるプログラム
+fn main() {
+    // 配列に文字列を代入
+    let array = [
+        "Apple".to_string(),
+        "Banana".to_string(),
+        "Mango".to_string(),
+        "Tomato".to_string(),
+    ];
+    // 配列を繰り返し画面に表示したいが。。
+    for a in array { // ここで所有権がムーブしてしまう
+        println!("{}", a);
+    }
+    println!("len={}", array.len()); // ここでコンパイルエラー
+}
